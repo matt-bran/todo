fc241117541859c3bed79ad13a7b04719c183084
Matthew Brandao <brandaomatt1999@gmail.com>
2024-01-07 02:24:19 -0500

    implement sorted task insert, updated task item GUI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { taskList } from "./task";
+import { format } from "date-fns";
+import { isEqual } from "date-fns";
 
 const menu_list = document.getElementById('menu-list');
 const content_h_title = document.getElementById('content-header-title');
@@ -6,6 +8,7 @@ const overlay = document.getElementById('overlay');
 const add_task_btn = document.getElementById('add-task');
 const close_btn = document.getElementById('close-btn');
 const form = document.getElementById('new-task-form');
+const ul_tasks = document.getElementById('tasks');
 
 
 let lists_container = [];
@@ -36,12 +39,115 @@ close_btn.addEventListener('click', () => {
     overlay.style.display = "none";
 });
 
+ul_tasks.addEventListener('click', (e) => {
+    console.log(e.target);
+    if (e.target && e.target.matches("span.expand")) {
+        const btm_desc = e.target.parentNode.parentNode.parentNode.children.item(1);
+        if (btm_desc.textContent == "No description available...") {
+            btm_desc.style.color = "#888";
+        }
+        if (e.target.classList.contains('open')){
+            e.target.classList.remove('open');
+            e.target.parentNode.parentNode.parentNode.children.item(1).classList.add('hidden-desc');
+        } else {
+            e.target.parentNode.parentNode.parentNode.children.item(1).classList.remove('hidden-desc');
+            e.target.classList.add('open');
+        }
+    }
+});
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
 
+    const input_title = formData.get("title");
+    const input_date = formData.get("duedate");
+    const input_priority = formData.get("priority");
+    let input_desc = formData.get("desc");
+    if (input_desc == "") {
+        input_desc = "No description available...";
+    }
+
+    const list_title = document.querySelector('.active').textContent;
+    for (let i = 0; i < lists_container.length; i++) {
+        if (lists_container[i].getTitle() == list_title) {
+            lists_container[i].insert(input_title, input_desc, input_date, input_priority);
+            refreshDOMList(lists_container[i]);
+        }
+    }
     form.reset();
     overlay.style.display = "none";
 });
+
+function refreshDOMList(list) {
+    ul_tasks.innerHTML = '';
+    for (let i = 0; i < list.getSize(); i++) {
+        const li = document.createElement('li');
+        li.classList.add('item');
+        
+        const p_title = document.createElement('p');
+        const text = list.getElementAt(i).getTitle();
+        p_title.textContent = text;
+
+        const task_right = document.createElement('div');
+        task_right.classList.add('task-right');
+        const task_left = document.createElement('div');
+        task_left.classList.add('task-left');
+
+        const p_date = document.createElement('p');
+        p_date.classList.add('task-date');
+        const date = list.getElementAt(i).getDueDate();
+        if (!isEqual(date, new Date(0))) {
+            p_date.textContent = "due " + format(date, 'MM/dd/yyyy');
+            task_right.appendChild(p_date);
+        }
+
+        const priority = list.getElementAt(i).getPriority();
+        const priority_p = document.createElement('p');
+        switch (priority) {
+            case 0: 
+                priority_p.style.color = "#888";
+                priority_p.textContent = 'Low';
+                break;
+            case 1: 
+                priority_p.style.color = "#0baeb9";
+                priority_p.textContent = 'Medium';
+                break;
+            case 2: 
+                priority_p.style.color = "#b84646";
+                priority_p.textContent = 'High';
+                break;
+        }
+        const svg_sq = document.createElement('span');
+        svg_sq.classList.add("material-symbols-outlined");
+        svg_sq.classList.add("square");
+        svg_sq.textContent = "edit_square";
+
+        const svg_expand = document.createElement('span');
+        svg_expand.classList.add("material-symbols-outlined");
+        svg_expand.classList.add("expand");
+        svg_expand.textContent = "expand_more";
+
+        const desc = document.createElement('p');
+        desc.classList.add('hidden-desc');
+        desc.classList.add('desc');
+        desc.textContent = list.getElementAt(i).getDescription();
+
+        const li_top = document.createElement('div');
+        li_top.classList.add('li-top');
+        
+
+        task_left.appendChild(svg_expand);
+        task_left.appendChild(p_title);
+        task_right.appendChild(priority_p);
+        task_right.appendChild(svg_sq);
+        li_top.appendChild(task_left);
+        li_top.appendChild(task_right);
+        li.appendChild(li_top);
+        li.appendChild(desc);
+
+        ul_tasks.appendChild(li);
+    }
+}
 
--- src/task.js
+++ src/task.js
@@ -1,3 +1,9 @@
+import { isEqual } from "date-fns";
+
+const LOW = 0;
+const MED = 1;
+const HIGH = 2;
+
 
 const task = (title, description, dueDate, priority) => {
     const task_title = title;
@@ -26,12 +32,58 @@ const taskList = (title) => {
     const list_title = title;
     var tasks = [];
 
-    const add = (task_title, description, dueDate, priority) => 
-        tasks.push(task(task_title, description, dueDate, priority));
+    // sorted insert by priority
+    const insert = (task_title, description, dueDate, priority) => {
+        let ins_prior = -1;
+        let ins_date = new Date(0);
+        if (dueDate != "") {
+            const args = dueDate.split('-')
+            ins_date = new Date(args[0], args[1], args[2]);
+        }
+        console.log(priority);
+        switch (priority) {
+            case 'low': 
+                ins_prior = LOW;
+                break;
+            case 'medium': 
+                ins_prior = MED;
+                break;
+            case 'high': 
+                ins_prior = HIGH;
+        }
+        if (tasks.length == 0) {
+            tasks.push(task(task_title, description, ins_date, ins_prior));
+            return;
+        }
+        for (let i = 0; i < tasks.length; i++) {
+            let temp_prior = tasks[i].getPriority();
+            if (ins_prior > temp_prior) {
+                tasks.splice(i, 0, task(task_title, description, ins_date, ins_prior));
+                return;
+            } 
+            else {
+                if (i == tasks.length-1) {
+                    tasks.push(task(task_title, description, ins_date, ins_prior));
+                    return;
+                }
+                else if (ins_prior > tasks[i+1].getPriority) {
+                    tasks.splice(i+1, 0, task(task_title, description, ins_date, ins_prior));
+                    return;
+                }
+            } 
+        }
+    };
+
     const remove = (task) => list.remove(task);
+
     const getTitle = () => list_title;
 
-    return { add, remove, getTitle }
+    const getElementAt = (index) => tasks[index];
+
+    const getSize = () => tasks.length;
+    
+
+    return { insert, remove, getTitle, getElementAt, getSize }
 } 
 
 export { taskList }
