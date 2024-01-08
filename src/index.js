06dca3872a22cae3753d4a26c6ab2e0f877f2518
Matthew Brandao <brandaomatt1999@gmail.com>
2024-01-07 20:27:28 -0500

    create new lists, resresh display upin changing lists

--- src/index.js
+++ src/index.js
@@ -9,9 +9,13 @@ const add_task_btn = document.getElementById('add-task');
 const close_btn = document.getElementById('close-btn');
 const form = document.getElementById('new-task-form');
 const ul_tasks = document.getElementById('tasks');
+const add_list = document.getElementById('add-list');
+const new_list_form = document.getElementById('new-list-form')
+const cancel_list = document.getElementById('cancel-list');
 
 
-let lists_container = [];
+//let lists_container = [];
+let lists_container = {};
 
 if (window.localStorage.length == 0){
     init();
@@ -20,16 +24,30 @@ if (window.localStorage.length == 0){
 }
 
 function init() {
-    const task_list = taskList('Uncategorized');
-    lists_container.push(task_list);
-
-    const li = document.createElement('li');
-    li.classList.add('active');
-    li.classList.add('list-item');
-    li.textContent = task_list.getTitle();
-    content_h_title.textContent = task_list.getTitle();
-    menu_list.appendChild(li);
+    console.log("init does nothing");
 }
+
+new_list_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(new_list_form);
+    const new_list_title = formData.get("new-list-title");
+    lists_container[new_list_title] = taskList(new_list_title);
+    add_list.classList.remove('hidden');
+    new_list_form.classList.add('hidden');
+    const menu_li = document.createElement('li');
+    menu_li.textContent = new_list_title;
+    menu_list.append(menu_li);
+});
+
+add_list.addEventListener('click', () => {
+    add_list.classList.add('hidden');
+    add_list.nextElementSibling.classList.remove('hidden');
+});
+
+cancel_list.addEventListener('click', () => {
+    cancel_list.parentElement.parentElement.previousElementSibling.classList.remove('hidden');
+    cancel_list.parentElement.parentElement.classList.add('hidden');
+});
 
 add_task_btn.addEventListener('click', () => {
     overlay.style.display = "block";
@@ -39,18 +57,27 @@ close_btn.addEventListener('click', () => {
     overlay.style.display = "none";
 });
 
+menu_list.addEventListener('click', (e) => {
+    if (e.target && e.target.matches('li')) {
+        refreshDOMList(lists_container[e.target.textContent]);
+        //menu_list.children.forEach((node) => node.classList.remove('active'));
+        Array.from(menu_list.children).forEach((node) => node.classList.remove('active'));
+        e.target.classList.add('active');
+    }
+});
+
 ul_tasks.addEventListener('click', (e) => {
-    console.log(e.target);
     if (e.target && e.target.matches("span.expand")) {
         const btm_desc = e.target.parentNode.parentNode.parentNode.children.item(1);
         if (btm_desc.textContent == "No description available...") {
             btm_desc.style.color = "#888";
+            btm_desc.style.fontStyle = "italic";
         }
         if (e.target.classList.contains('open')){
             e.target.classList.remove('open');
-            e.target.parentNode.parentNode.parentNode.children.item(1).classList.add('hidden-desc');
+            btm_desc.classList.add('hidden');
         } else {
-            e.target.parentNode.parentNode.parentNode.children.item(1).classList.remove('hidden-desc');
+            btm_desc.classList.remove('hidden');
             e.target.classList.add('open');
         }
     }
@@ -60,7 +87,6 @@ ul_tasks.addEventListener('click', (e) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
-
     const input_title = formData.get("title");
     const input_date = formData.get("duedate");
     const input_priority = formData.get("priority");
@@ -68,31 +94,27 @@ form.addEventListener('submit', (e) => {
     if (input_desc == "") {
         input_desc = "No description available...";
     }
-
     const list_title = document.querySelector('.active').textContent;
-    for (let i = 0; i < lists_container.length; i++) {
-        if (lists_container[i].getTitle() == list_title) {
-            lists_container[i].insert(input_title, input_desc, input_date, input_priority);
-            refreshDOMList(lists_container[i]);
-        }
-    }
+    lists_container[list_title].insert(input_title, input_desc, input_date, input_priority);
+    refreshDOMList(lists_container[list_title]);
     form.reset();
     overlay.style.display = "none";
 });
 
 function refreshDOMList(list) {
     ul_tasks.innerHTML = '';
+    content_h_title.textContent = list.getTitle();
     for (let i = 0; i < list.getSize(); i++) {
+        
         const li = document.createElement('li');
         li.classList.add('item');
         
         const p_title = document.createElement('p');
-        const text = list.getElementAt(i).getTitle();
-        p_title.textContent = text;
+        p_title.textContent = list.getElementAt(i).getTitle();
 
         const task_right = document.createElement('div');
-        task_right.classList.add('task-right');
         const task_left = document.createElement('div');
+        task_right.classList.add('task-right');
         task_left.classList.add('task-left');
 
         const p_date = document.createElement('p');
@@ -102,7 +124,6 @@ function refreshDOMList(list) {
             p_date.textContent = "due " + format(date, 'MM/dd/yyyy');
             task_right.appendChild(p_date);
         }
-
         const priority = list.getElementAt(i).getPriority();
         const priority_p = document.createElement('p');
         switch (priority) {
@@ -130,7 +151,7 @@ function refreshDOMList(list) {
         svg_expand.textContent = "expand_more";
 
         const desc = document.createElement('p');
-        desc.classList.add('hidden-desc');
+        desc.classList.add('hidden');
         desc.classList.add('desc');
         desc.textContent = list.getElementAt(i).getDescription();
 
