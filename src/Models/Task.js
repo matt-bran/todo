e26ac5d335cf546297005d77d00713b5d5bd7d0a
Matthew Brandao <brandaomatt1999@gmail.com>
2024-01-13 16:59:17 -0500

    updated variable titles

--- src/Models/Task.js
+++ src/Models/Task.js
@@ -1,23 +1,21 @@
-const Task = (title, description, dueDate, priority) => {
-    let task_title = title;
-    let task_description = description;
-    let task_dueDate = dueDate;
-    let task_priority = priority;
+const Task = (newTitle, newDesc, newDate, newPriority) => {
+    let title = newTitle;
+    let description = newDesc;
+    let dueDate = newDate;
+    let priority = newPriority;
     let isComplete = false;
 
-    const getTitle = () => task_title;
-    const getDescription = () => task_description; 
-    const getDueDate = () => task_dueDate;
-    const getPriority = () => task_priority;
+    const getTitle = () => title;
+    const getDescription = () => description; 
+    const getDueDate = () => dueDate;
+    const getPriority = () => priority;
     const getisComplete = () => isComplete;
 
-    const setTitle = (new_title) => { task_title = new_title };
-    const setDescription = (new_desc) => { task_description = new_desc }; 
-    const setDueDate = (new_date) => { task_dueDate = new_date };
-    const setPriority = (new_priority) => { task_priority = new_priority }; 
-    const toggleisComplete = () => {
-        isComplete = !isComplete;
-    };
+    const setTitle = (newTitle) => { title = newTitle };
+    const setDescription = (newDesc) => { description = newDesc }; 
+    const setDueDate = (newDate) => { dueDate = newDate };
+    const setPriority = (newPriority) => { priority = newPriority }; 
+    const toggleisComplete = () => { isComplete = !isComplete; };
     return { getTitle, getDescription, getDueDate, getPriority, setTitle, 
              setDescription, setDueDate, setPriority, getisComplete, toggleisComplete }
 };
